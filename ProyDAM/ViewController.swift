1552ca4afa9179ee253cc640e618f256f62c2ed6
krlosCibertec <i201822031@cibertec.edu.pe>
2023-10-07 18:20:08 -0500

    Initial Commit

---
+++ ProyDAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyDAM
+//
+//  Created by krlos90 on 7/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
