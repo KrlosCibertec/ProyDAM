d808d8f4e3ef31ff99746fe5a238ad9280365af8
krlosCibertec <i201822031@cibertec.edu.pe>
2023-10-07 18:50:10 -0500

    prueba one

---
+++ ProyDAM/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ProyDAM
+//
+//  Created by krlos90 on 7/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+
+    }
+   
+}
---
+++ ProyDAM/Controllers/UpCommingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpCommingViewController.swift
+//  ProyDAM
+//
+//  Created by krlos90 on 7/10/23.
+//
+
+import UIKit
+
+class UpCommingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
